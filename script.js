b718981165e45563c30ea974ce36717e5b6e9586
GshopTopup <148093936+GshopTopup@users.noreply.github.com>
2023-10-19 11:39:54 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,50 @@
-document.getElementById('vy').addEventListener('click', function(event) {
-    // Prevent the default form submission behavior
+document.getElementById('continue').addEventListener('click', function(event) {
     event.preventDefault();
 
-    // Check if at least one radio input is selected
-    var radioInputs = document.querySelectorAll('input[name="amount"]');
-    var selected = false;
-    for (var i = 0; i < radioInputs.length; i++) {
-      if (radioInputs[i].checked) {
-        selected = true;
-        break; // Exit the loop if a radio input is selected
+    // Check if an amount option is selected in the 'amount' section
+    var amountInputs = document.querySelectorAll('input[name="amount"]');
+    var amountSelected = false;
+    for (var i = 0; i < amountInputs.length; i++) {
+      if (amountInputs[i].checked) {
+        amountSelected = true;
+        break;
       }
     }
 
-    if (!selected) {
-      alert('Please select a recharge');
-      return; // Stop further execution if no radio input is selected
+    // Check if a payment method is selected in the 'logo2' section
+    var paymentInputs = document.querySelectorAll('input[name="payment"]');
+    var paymentSelected = false;
+    var selectedPaymentValue = null;
+    for (var i = 0; i < paymentInputs.length; i++) {
+      if (paymentInputs[i].checked) {
+        paymentSelected = true;
+        selectedPaymentValue = paymentInputs[i].value;
+        break;
+      }
     }
 
-    // Check if the Player ID input field is filled
+    // Check if the player ID (UID) is filled
     var playerIdInput = document.getElementById('in');
-    var transactionIdInput = document.getElementById('re2');
-
     if (playerIdInput.value.trim() === '') {
-      alert('Player ID (UID) Not Filled.');
-      return; // Stop further execution if the Player ID input is empty
+      alert('Player ID (UID) is not filled.');
+      return;
     }
 
-    if (transactionIdInput.value.trim() === '') {
-      alert('Transaction ID Not Filled.');
-      return; // Stop further execution if the Transaction ID input is empty
+    // Display different alerts based on the section that is not selected
+    if (!amountSelected && !paymentSelected) {
+      alert('Please select all options.');
+    } else if (!amountSelected) {
+      alert('Select a recharge.');
+    } else if (!paymentSelected) {
+      alert('Select Bkash or Nagad');
+    } else {
+      // Proceed to the next step if both conditions are met
+      if (selectedPaymentValue === 'bk') {
+        window.open('Bkash-pay.html', '_blank');
+      } else if (selectedPaymentValue === 'ng') {
+        window.open('Nagad-pay.html', '_blank');
+      } else {
+        alert('Invalid payment selection');
+      }
     }
-      // Redirect to a new page
-      window.location.href = 'thank.html'; // Replace 'new_page.html' with your desired page URL
-    }, 1000);
+  });
